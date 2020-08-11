7a2c2744d275ebb0840b8c91246d9b31baa429c7
jp-richter <janp.richter@web.de>
2020-08-11 10:36:24 +0200

    changed mind news path

--- crawler/msn/spiders/msn_spider.py
+++ crawler/msn/spiders/msn_spider.py
@@ -9,20 +9,20 @@ class MSNSpider(scrapy.Spider):
     name = "msn"
     allowed_domains = ["msn.com"]
 
-    # start_urls = []
-    # with open(os.environ["MIND_NEWS_PATH"], 'r') as f:
-    #     for l in f:
-    #         _, _, _, _, _, url, _, _ = l.strip('\n').split('\t')
-    #         start_urls.append(url)
+    start_urls = []
+    with open(os.environ["MIND_NEWS_PATH"], 'r') as f:
+        for l in f:
+            _, _, _, _, _, url, _, _ = l.strip('\n').split('\t')
+            start_urls.append(url)
 
-    start_urls = [
-        # ss
-        "https://mind201910small.blob.core.windows.net/archive/AAGH0ET.html",
-        # ar
-        "https://mind201910small.blob.core.windows.net/archive/AABmf2I.html",
-        # vi
-        "https://mind201910small.blob.core.windows.net/archive/AAI33em.html"
-    ]
+    # start_urls = [
+    #     # ss
+    #     "https://mind201910small.blob.core.windows.net/archive/AAGH0ET.html",
+    #     # ar
+    #     "https://mind201910small.blob.core.windows.net/archive/AABmf2I.html",
+    #     # vi
+    #     "https://mind201910small.blob.core.windows.net/archive/AAI33em.html"
+    # ]
 
     def __init__(self):
         with open('./doc_type.json', 'r') as f:
